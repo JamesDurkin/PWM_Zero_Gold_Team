bcf3c0c3e5564ddbb6cf90cdb4dd2d6fe283e8be
JamesDurkin <31664364+JamesDurkin@users.noreply.github.com>
2017-11-07 14:50:35 -0500

    Added Varible resitor to Zero to control PWM

---
+++ PWM_Zero_Gold_Team_part2.ino
@@ -0,0 +1,25 @@
+
+
+int analogPin = A0;
+
+int pot_ratio;
+int pot;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(analogPin, INPUT);
+  analogReadResolution(10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(LED_BUILTIN, pot_scale);
+pot = analogRead(A0);
+delay(100);
+
+pot_ratio= pot / 8;
+
+
+}
