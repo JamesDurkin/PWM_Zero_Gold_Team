8d6b873eeb5db142f2e236fa345e95d2aa6f8e8a
JamesDurkin <31664364+JamesDurkin@users.noreply.github.com>
2017-11-07 14:27:39 -0500

    Add files via upload
    First Part of the lab.

---
+++ PWM_Zero_Gold_Team.ino
@@ -0,0 +1,30 @@
+const int MAX_8_INT = 255;
+
+int brightness;
+
+int digitalPin = 6;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  int brightness = 0;
+
+  int fade_amount = 1;
+
+  pinMode(digitalPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(LED_BUILTIN, pot);
+
+brightness = brightness + fade_amount;
+
+if(brightness == 0 or brightness == MAX_8_INT) fade_amount =- fade_amount;
+
+delay(100);
+
+
+}
